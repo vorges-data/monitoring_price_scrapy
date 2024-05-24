3403bad809d364d914aee3e60855909d806d3807
vorges-data <vinicius.jborges36@gmail.com>
2024-05-23 22:50:14 -0300

    Adiciando arquivo .ipynb para testar feature engineering

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -12,16 +12,21 @@ class MercadolivreSpider(scrapy.Spider):
         products = response.css('div.ui-search-result__content')
 
         for product in products:
-            
-            prices = products.css('span.andes-money-amount__fraction::text').getall()    
-            cents = products.css('span.andes-money-amount__cents.andes-money-amount__cents--superscript-16::text').getall()
-
+            old_price = product.css('s.andes-money-amount')
+            if old_price:
+                old_price_reais = old_price.css('span.andes-money-amount__fraction::text').get()
+                old_price_cents = old_price.css('span.andes-money-amount__cents::text').get()
+                new_price_reais = product.css('div.ui-search-price__second-line span.andes-money-amount__fraction::text').get()
+            else:
+                old_price_reais = None
+                old_price_cents = None
+                new_price_reais = product.css('span.andes-money-amount__fraction::text').get()
+                
             yield {
                 "brand": product.css('h2.ui-search-item__title::text').get(),
-                "old_price_reais": prices[0] if len(prices) > 0 else None,
-                "old_price_centavos": cents[0] if len(cents) > 0 else None,
-                "new_price_reais": prices[1] if len(prices) > 1 else None,
-                "new_price_centavos": cents[1] if len(cents) > 1 else None,
+                "old_price_reais": old_price_reais,
+                "old_price_centavos": old_price_cents,
+                "new_price_reais": new_price_reais,
                 "reviews_rating_number": product.css('span.ui-search-reviews__rating-number::text').get(),
                 "reviews_amount": product.css('span.ui-search-reviews__amount::text').get()
             }
@@ -30,4 +35,4 @@ class MercadolivreSpider(scrapy.Spider):
             next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
             if next_page:
                 self.page_count += 1
-                yield scrapy.Request(url = next_page, callback=self.parse)
+                yield scrapy.Request(url=next_page, callback=self.parse)
