cd0c73af2633816ff1f0248710c9a309d71cdc21
vorges-data <vinicius.jborges36@gmail.com>
2024-05-25 18:44:27 -0300

    Codigo refatorado para a forma POO

--- src/transform/main.py
+++ src/transform/main.py
@@ -2,98 +2,105 @@ import pandas as pd
 import sqlite3
 from datetime import datetime
 import re
-import os
 
-# Path arquivo JSON
-df = pd.read_json('../../data/data.jsonl', lines=True)
+class DataTransformer:
+    def __init__(self, input_path, output_db_path):
+        self.input_path = input_path
+        self.output_db_path = output_db_path
+        self.data = None
 
-# Setar o pandas para mostrar todas as colunas
-pd.set_option('display.max_columns', None)
+    def load_data(self):
+        self.data = pd.read_json(self.input_path, lines=True)
+        print("Data loaded successfully")
 
-# Adicionar coluna fonte dos dados
-df['_source'] = "https://lista.mercadolivre.com.br/notebook#D[A:notebook]"
+    def set_options(self):
+        pd.set_option('display.max_columns', None)
 
-# Adicionar coluna data de coleta
-df['_data_coleta'] = datetime.now()
+    def add_columns(self):
+        self.data['_source'] = "https://lista.mercadolivre.com.br/notebook#D[A:notebook]"
+        self.data['_data_coleta'] = datetime.now()
 
-# Remover pontos dos valores de preços e converter para inteiros
-df['new_price_reais'] = df['new_price_reais'].astype(str).str.replace('.', '').astype(float)
-df['old_price_reais'] = df['old_price_reais'].astype(str).str.replace('.', '').astype(float)
-df['old_price_centavos'] = df['old_price_centavos'].astype(str).str.replace('.', '').astype(float)
+    def clean_prices(self):
+        self.data['new_price_reais'] = self.data['new_price_reais'].astype(str).str.replace('.', '').astype(float)
+        self.data['old_price_reais'] = self.data['old_price_reais'].astype(str).str.replace('.', '').astype(float)
+        self.data['old_price_centavos'] = self.data['old_price_centavos'].astype(str).str.replace('.', '').astype(float)
 
-# Garantir que todos os preços sejam tratados como float
-df['new_price_reais'] = pd.to_numeric(df['new_price_reais'], errors='coerce')
-df['old_price_reais'] = pd.to_numeric(df['old_price_reais'], errors='coerce')
-df['old_price_centavos'] = pd.to_numeric(df['old_price_centavos'], errors='coerce')
-df['reviews_rating_number'] = pd.to_numeric(df['reviews_rating_number'], errors='coerce')
+    def convert_columns(self):
+        self.data['new_price_reais'] = pd.to_numeric(self.data['new_price_reais'], errors='coerce')
+        self.data['old_price_reais'] = pd.to_numeric(self.data['old_price_reais'], errors='coerce')
+        self.data['old_price_centavos'] = pd.to_numeric(self.data['old_price_centavos'], errors='coerce')
+        self.data['reviews_rating_number'] = pd.to_numeric(self.data['reviews_rating_number'], errors='coerce')
+        self.data['old_price_reais'] = self.data['old_price_reais'].fillna(0).astype(float)
+        self.data['old_price_centavos'] = self.data['old_price_centavos'].fillna(0).astype(float)
+        self.data['new_price_reais'] = self.data['new_price_reais'].fillna(0).astype(float)
+        self.data['reviews_rating_number'] = self.data['reviews_rating_number'].fillna(0).astype(float)
 
-# Transformar a tipagem das colunas
-df['old_price_reais'] = df['old_price_reais'].fillna(0).astype(float)
-df['old_price_centavos'] = df['old_price_centavos'].fillna(0).astype(float)
-df['new_price_reais'] = df['new_price_reais'].fillna(0).astype(float)
-#df['new_price_centavos'] = df['new_price_centavos'].fillna(0).astype(float) # Não há notebook com centavos no preço
-df['reviews_rating_number'] = df['reviews_rating_number'].fillna(0).astype(float)
+    def clean_reviews(self):
+        self.data['reviews_amount'] = self.data['reviews_amount'].str.replace('[\(\)]', '', regex=True)
+        self.data['reviews_amount'] = self.data['reviews_amount'].fillna(0).astype(int)
 
-# Remover os parenteses do reviews_amount
-df['reviews_amount'] = df['reviews_amount'].str.replace('[\(\)]', '', regex=True)
-df['reviews_amount'] = df['reviews_amount'].fillna(0).astype(int)
+    def calculate_prices(self):
+        self.data['old_price'] = self.data['old_price_reais'] + self.data['old_price_centavos'] / 100
+        self.data = self.data.drop(columns=['old_price_reais', 'old_price_centavos'])
 
-# Tratar os preços
-df['old_price'] = df['old_price_reais'] + df['old_price_centavos'] / 100
-#df['new_price'] = df['new_price_reais'] + df['new_price_centavos'] / 100 # Não há notebook com centavos no preço
+    def calculate_discount(self):
+        self.data['discount_percentage'] = self.data.apply(
+            lambda row: ((row['old_price'] - row['new_price_reais']) / row['old_price']) * 100
+            if row['old_price'] not in [None, 0] else 0,
+            axis=1
+        )
 
-# Remover as colunas de preço antigas
-df = df.drop(columns=['old_price_reais', 'old_price_centavos'])
+    def extract_brand(self):
+        marcas = ['Apple', 'Samsung', 'Dell', 'Asus', 'Lenovo', 'HP', 'Acer', 'Microsoft', 'Toshiba', 'Sony', 'MSI', 'Razer','LG','Huawei','Xiaomi','Google','Panasonic','Vaio','Positivo','Multilaser']
+        def extrair_marca(brand_name):
+            for marca in marcas:
+                if re.search(marca, brand_name, re.IGNORECASE):
+                    return marca
+            return 'Outras'
+        self.data['marca'] = self.data['brand'].apply(extrair_marca)
 
-# Calcular percentual de desconto
-df['discount_percentage'] = df.apply(
-    lambda row: ((row['old_price'] - row['new_price_reais']) / row['old_price']) * 100
-    if row['old_price'] not in [None, 0] else 0,
-    axis=1
-)
+    def categorize_discount(self):
+        def categorize(row):
+            if row['discount_percentage'] == 0:
+                return 'Sem desconto'
+            elif 0 < row['discount_percentage'] <= 10:
+                return 'Até 10%'
+            elif 11 <= row['discount_percentage'] <= 20:
+                return '11% a 20%'
+            elif 21 <= row['discount_percentage'] <= 30:
+                return '21% a 30%'
+            elif 31 <= row['discount_percentage'] <= 40:
+                return '31% a 40%'
+            elif 41 <= row['discount_percentage'] <= 50:
+                return '41% a 50%'
+            else:
+                return 'Acima de 50%'
+        self.data['discount_range'] = self.data.apply(categorize, axis=1)
 
-# Definir lista de marcas conhecidas
-marcas = ['Apple', 'Samsung', 'Dell', 'Asus', 'Lenovo', 'HP', 'Acer', 'Microsoft', 'Toshiba', 'Sony', 'MSI', 'Razer','LG','Huawei','Xiaomi','Google','Panasonic','Vaio','Positivo','Multilaser']
+    def reorder_columns(self):
+        nova_ordem = ['brand','marca','old_price','new_price_reais','discount_percentage','discount_range','reviews_amount','reviews_rating_number','supplier','installments','product_url','img_product_url','_source','_data_coleta']
+        self.data = self.data[nova_ordem]
 
-# Função para extrair a marca do notebook
-def extrair_marca(brand_name):
-    for marca in marcas:
-        if re.search(marca, brand_name, re.IGNORECASE):
-            return marca
-    return 'Outras'
+    def save_to_db(self):
+        conn = sqlite3.connect(self.output_db_path)
+        self.data.to_sql('mercadolivre_items', conn, if_exists='replace', index=False)
+        conn.close()
+        print("Data saved to database successfully")
 
-# Aplicar a função ao DataFrame
-df['marca'] = df['brand'].apply(extrair_marca)
+    def transform(self):
+        self.load_data()
+        self.set_options()
+        self.add_columns()
+        self.clean_prices()
+        self.convert_columns()
+        self.clean_reviews()
+        self.calculate_prices()
+        self.calculate_discount()
+        self.extract_brand()
+        self.categorize_discount()
+        self.reorder_columns()
+        self.save_to_db()
 
-
-# Faixa de desconto
-def categorize_discount(row):
-    if row['discount_percentage'] == 0:
-        return 'Sem desconto'
-    elif 0 < row['discount_percentage'] <= 10:
-        return 'Até 10%'
-    elif 11 <= row['discount_percentage'] <= 20:
-        return '11% a 20%'
-    elif 21 <= row['discount_percentage'] <= 30:
-        return '21% a 30%'
-    elif 31 <= row['discount_percentage'] <= 40:
-        return '31% a 40%'
-    elif 41 <= row['discount_percentage'] <= 50:
-        return '41% a 50%'
-    else:
-        return 'Acima de 50%'
-# Criar a nova coluna com as faixas de desconto
-df['discount_range'] = df.apply(categorize_discount, axis=1)
-
-# Reordenar as colunas
-nova_ordem = ['brand','marca','old_price','new_price_reais','discount_percentage','discount_range','reviews_amount','reviews_rating_number','supplier','installments','product_url','img_product_url','_source','_data_coleta']
-df = df[nova_ordem]
-
-# Conectar ao banco de dados
-conn = sqlite3.connect('../../data/price_notebooks_ml.db')
-
-# Salvar o dataframe no banco de dados
-df.to_sql('mercadolivre_items', conn, if_exists='replace', index=False)
-
-# Fechar a conexão
-conn.close()
+if __name__ == "__main__":
+    transformer = DataTransformer('../../data/data.jsonl', '../../data/price_notebooks_ml.db')
+    transformer.transform()
